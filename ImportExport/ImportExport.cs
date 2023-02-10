1792e22d71b79dc68e3fd5c515fdbb5919678845
Zami16 <yusufazzamzami@gmail.com>
2023-02-10 21:56:09 +0700

    Add Function to import DataSet

--- ImportExport/ImportExport.cs
+++ ImportExport/ImportExport.cs
@@ -9,6 +9,48 @@ namespace AxataPOS.ImportExport
     public static class ImportExport
     {
         /// <summary>
+        /// Get DataSet from excel
+        /// </summary>
+        /// <param name="FileName">Excel File</param>
+        /// <returns>DataSet</returns>
+        public static DataSet ImportExcel2DataSet(string FileName)
+        {
+            IExcelDataReader rdr;
+            try
+            {
+                string ext = Path.GetExtension(FileName);
+                var ms = File.Open(FileName, FileMode.Open, FileAccess.Read);
+                switch (ext)
+                {
+                    case ".xls": //Old style (2003 or less)
+                        rdr = ExcelReaderFactory.CreateBinaryReader(ms);
+                        break;
+                    case ".xlsx": //New style (2007 or higher)
+                        rdr = ExcelReaderFactory.CreateOpenXmlReader(ms);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(String.Format("File extension {0} is not recognized as valid", ext));
+                }
+
+                using (DataSet ds = rdr.AsDataSet(new ExcelDataSetConfiguration
+                {
+                    UseColumnDataType = true,
+                    ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                    {
+                        UseHeaderRow = true
+                    }
+                }))
+                {
+                    return ds;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
         /// Import Data From Excel to DataTable
         /// </summary>
         /// <param name="FileName">Nama file excel</param>
@@ -57,6 +99,24 @@ namespace AxataPOS.ImportExport
         /// <param name="data">DataTable</param>
         /// <param name="FilePath">Nama dan Path dari file</param>
         public static void ExportToExcel(DataTable data, string FilePath)
+        {
+            try
+            {
+                var excel = data.ToExcel();
+                File.WriteAllBytes(FilePath, excel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Export DataSet to excel file
+        /// </summary>
+        /// <param name="data">DataSet</param>
+        /// <param name="FilePath">Excel File Name </param>
+        public static void ExportToExcel(DataSet data, string FilePath)
         {
             try
             {
--- ImportExportConsole/Program.cs
+++ ImportExportConsole/Program.cs
@@ -1,13 +1,50 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
+using FirebirdSql.Data.FirebirdClient;
 
 namespace ImportExportConsole
 {
     internal class Program
     {
+        private static readonly string FileTest = @"D:\KERJA\AXATA\ImportExport\TestImportFromData.xlsx";
+        private static readonly string StrCon = $@"database=192.168.0.73:C:\Users\yusuf\OneDrive\Desktop\Axata\DB\AxataPOS\EMAS\Debug\Axata.axt;
+            user=SYSDBA; Password=masterkey; Connection lifetime=0; Dialect=3; Server=localhost";
+        private static readonly FbConnection conn = new FbConnection(StrCon);
         public static void Main()
         {
+            /*DataSet ds = AxataPOS.ImportExport.ImportExport.ImportExcel2DataSet(FileTest);
+            foreach (DataTable dataTable in ds.Tables)
+            {
+                Console.WriteLine(dataTable.TableName);
+            }
+            AxataPOS.ImportExport.ImportExport.ExportToExcel(ds, @"D:\KERJA\AXATA\ImportExport\test-from-import-ds.xlsx");
+            
             DataTable dt = AxataPOS.ImportExport.ImportExport.ImportExcel(@"D:\KERJA\AXATA\ImportExport\test.xlsx");
-            AxataPOS.ImportExport.ImportExport.ExportToExcel(dt, @"D:\KERJA\AXATA\ImportExport\test-from-import.xlsx");
+            AxataPOS.ImportExport.ImportExport.ExportToExcel(dt, @"D:\KERJA\AXATA\ImportExport\test-from-import.xlsx");*/
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            
+
+            DataTable dt = new DataTable();
+            using (var con = conn)
+            {
+                using (var cmd = new FbCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection= con;
+                    cmd.CommandText = "select * from barang";
+                    using (var da = new FbDataAdapter(cmd))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+            }
+            AxataPOS.ImportExport.ImportExport.ExportToExcel(dt, FileTest);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.ReadLine();
         }
     }
 }
